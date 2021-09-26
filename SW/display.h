7ccce6bb8ebc0cb7cea894e306106551ee6db794
cybaer <s.k.g@gmx.de>
2021-09-26 12:45:00 +0200

    decimal point added

--- SW/display.h
+++ SW/display.h
@@ -28,33 +28,32 @@ static const int8_t BrightnessLevels = 4;
 
 extern const uint8_t* LUT_7Seg_Character;
 
-template<typename GPIO, int8_t Digits, setFct setD1, setFct setD2, setFct setD3>
+template<typename GPIO, int8_t Digits, uint8_t DP, setFct setD1, setFct setD2, setFct setD3>
 class Display_7_Seg
 {
  public:
   static inline void Init()
   {
-    
+    resetAllDP();
     m_ActivePosition = 0;
   }
 
   static inline void Refresh()
   {
-    //if (brightness_pwm_cycle_ <= m_Brightness) 
-    //{
+    if (m_BrightnessCycle <= m_Brightness) 
+    {
       DigitIO[m_ActivePosition](false);
       m_ActivePosition = (m_ActivePosition + 1) % Digits;
-      //writeDigit(/*chars[static_cast<uint8_t>*/(m_Buffer[m_ActivePosition])]);
-      writeDigit(LUT_7Seg_Character[m_Buffer[m_ActivePosition]]);
+      writeDigit(LUT_7Seg_Character[m_Buffer[m_ActivePosition]] | m_DecimalPoints[m_ActivePosition]);
 
       DigitIO[m_ActivePosition](true);
-    /*} else 
+    }
+    else 
     {
-    GPIOB->BRR = kCharacterEnablePins[active_position_];
-    }*/
-    //brightness_pwm_cycle_ = (brightness_pwm_cycle_ + 1) % kBrightnessLevels;
+      DigitIO[m_ActivePosition](false);
+    }
+    m_BrightnessCycle = (m_BrightnessCycle + 1) % BrightnessLevels;
   }
-
 
   static inline void Print(const char* s)
   {
@@ -72,6 +71,31 @@ class Display_7_Seg
     else Print("Err");
   }
 
+  static inline void setDP(int8_t idx, bool dp)
+  {
+    if(idx < Digits)
+    {
+      m_DecimalPoints[idx] = dp ? 0 : DP;
+    }
+  }
+
+  static inline void setDPs(uint8_t dps)
+  {
+    for(int8_t i=0; i<Digits; i++)
+    {
+      uint8_t mask = 0x4 >> i;
+      setDP(i, dps & mask);
+    }
+  }
+
+  static inline void resetAllDP()
+  {
+    for(int8_t i=0; i<Digits; i++)
+    {
+      setDP(i, false);
+    }
+  }
+
   static inline void Print(const char* s, const uint8_t v)
   {
     Print(v);
@@ -79,7 +103,7 @@ class Display_7_Seg
   }
 
   static inline char* mutable_buffer() { return m_Buffer; }
-  static inline void setBrightness(int8_t brightness) { m_Brightness = brightness; }
+  static inline void setBrightness(int8_t brightness) { m_Brightness = brightness < BrightnessLevels ? brightness : BrightnessLevels ; }
 
  private:
   static inline void writeDigit(uint8_t data)
@@ -90,22 +114,29 @@ class Display_7_Seg
 
   static setFct DigitIO[Digits];
   static char m_Buffer[Digits];
+  static char m_DecimalPoints[Digits];
   static int8_t m_ActivePosition;
-  //static int8_t brightness_pwm_cycle_;
+  static int8_t m_BrightnessCycle;
   static int8_t m_Brightness;
 
 };
 
-template<typename GPIO, int8_t Digits, setFct setD1, setFct setD2, setFct setD3>
-setFct Display_7_Seg<GPIO, Digits, setD1, setD2, setD3>::DigitIO[Digits] = {setD1, setD2, setD3};
+template<typename GPIO, int8_t Digits, uint8_t DP, setFct setD1, setFct setD2, setFct setD3>
+setFct Display_7_Seg<GPIO, Digits, DP, setD1, setD2, setD3>::DigitIO[Digits] = {setD1, setD2, setD3};
 
-template<typename GPIO, int8_t Digits, setFct setD1, setFct setD2, setFct setD3>
-char Display_7_Seg<GPIO, Digits, setD1, setD2, setD3>::m_Buffer[Digits] = {0};
+template<typename GPIO, int8_t Digits, uint8_t DP, setFct setD1, setFct setD2, setFct setD3>
+char Display_7_Seg<GPIO, Digits, DP, setD1, setD2, setD3>::m_Buffer[Digits] = {0};
 
-template<typename GPIO, int8_t Digits, setFct setD1, setFct setD2, setFct setD3>
-int8_t Display_7_Seg<GPIO, Digits, setD1, setD2, setD3>::m_ActivePosition = {0};
+template<typename GPIO, int8_t Digits, uint8_t DP, setFct setD1, setFct setD2, setFct setD3>
+char Display_7_Seg<GPIO, Digits, DP, setD1, setD2, setD3>::m_DecimalPoints[Digits] = {DP, DP, DP};
 
-template<typename GPIO, int8_t Digits, setFct setD1, setFct setD2, setFct setD3>
-int8_t Display_7_Seg<GPIO, Digits, setD1, setD2, setD3>::m_Brightness = {BrightnessLevels - 1};
+template<typename GPIO, int8_t Digits, uint8_t DP, setFct setD1, setFct setD2, setFct setD3>
+int8_t Display_7_Seg<GPIO, Digits, DP, setD1, setD2, setD3>::m_ActivePosition = {0};
+
+template<typename GPIO, int8_t Digits, uint8_t DP, setFct setD1, setFct setD2, setFct setD3>
+int8_t Display_7_Seg<GPIO, Digits, DP, setD1, setD2, setD3>::m_BrightnessCycle = {0};
+
+template<typename GPIO, int8_t Digits, uint8_t DP, setFct setD1, setFct setD2, setFct setD3>
+int8_t Display_7_Seg<GPIO, Digits, DP, setD1, setD2, setD3>::m_Brightness = {BrightnessLevels - 1};
 
 #endif
